218545f720f70b626be5efb605d4707e1f3caf8c
SagiSan <samir.sagi@gmail.com>
2019-11-07 13:10:12 +0100

    shop page

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
+
+const MenuItem = ({ title, imageUrl, linkUrl, size, history, match }) => {
+  console.log(match);
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      ></div>
+      <div className="content">
+        <div className="title">{title.toUpperCase()}</div>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(MenuItem);
