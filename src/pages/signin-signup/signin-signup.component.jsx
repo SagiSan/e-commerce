ae41026aae33643c53a4f484aa7fd2a67fe229d6
SagiSan <samir.sagi@gmail.com>
2019-11-12 13:36:44 +0100

    signup page, email and password

--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./signin-signup.styles.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const SignInSignUpPage = () => {
   return (
     <div className="signin-signup">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
