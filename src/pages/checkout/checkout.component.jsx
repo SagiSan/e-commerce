72b4bfeeea885c5c982e9af61915bbbfa90dd88e
SagiSan <samir.sagi@gmail.com>
2019-12-27 10:06:51 +0100

    stripe checkout

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
 } from "../../redux/cart/cart.selectors";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({ cartItems, total }) => {
   return (
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       <div className="total">
         <span>Total: ${total}</span>
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card from payments*
+        <br />
+        4242 4242 4242 4242 - Exp 01/20 - CVV: 123
+      </div>
+      <StripeButton price={total} />
     </div>
   );
 };
