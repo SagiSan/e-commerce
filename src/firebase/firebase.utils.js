633cbf9e8fa341f77d89533b7908601ccab2ed03
SagiSan <samir.sagi@gmail.com>
2019-12-29 17:12:08 +0100

    shop data on firestore to reducer and with spinner

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -32,6 +32,39 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
   return userRef;
 };
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+
+  objectsToAdd.forEach(objToAdd => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, objToAdd);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 firebase.initializeApp(config);
 
